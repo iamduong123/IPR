b412766e2772181bb5234583bf77ace9c4fdd970
iamduong123 <htduong.22003@gmail.com>
2024-02-02 09:50:11 +0700

    Initial commit

---
+++ TUT1/tut1.py
@@ -0,0 +1,70 @@
+from PIL import Image, ImageEnhance, ImageFilter
+
+def convert_to_grayscale(image_path, output_path):
+    img = Image.open(image_path)
+    gray_img = img.convert('L')
+    gray_img.save(output_path)
+    print(f"Grayscale image saved as {output_path}")
+convert_to_grayscale("public/tiger.jpg", "public/tiger_gray.jpg")
+
+def resize_img(image_path, output_path):
+    img = Image.open(image_path)
+    new_size = (75,90)
+    re_img = img.resize(new_size)
+    re_img.save(output_path)
+    print(f"Resize image saved as {output_path}")
+resize_img("public/tiger.jpg", "public/tiger_rs.jpg")
+
+def crop_img(image_path, output_path):
+    img = Image.open(image_path)
+    crop_box = (100, 100, 400, 400)
+    crp_img = img.crop(crop_box)
+    crp_img.save(output_path)
+    print(f"crop image saved as {output_path}")
+crop_img("public/tiger.jpg", "public/tiger_crp.jpg")
+
+def enhanced_img(image_path, output_path):
+    img = Image.open(image_path)
+    contrast_factor = 1.5  # Adjust as needed, 1.0 means no change
+    contrast = ImageEnhance.Contrast(img)
+    image_with_contrast = contrast.enhance(contrast_factor)
+    brightness_factor = 1.2  # Adjust as needed, 1.0 means no change
+    brightness = ImageEnhance.Brightness(image_with_contrast)
+    image_with_brightness = brightness.enhance(brightness_factor)
+    image_with_brightness.save(output_path)
+    print(f"enhanced image saved as {output_path}")
+enhanced_img("public/tiger.jpg", "public/tiger_enh.jpg")
+
+def rotate_and_flip_image(image_path, save_path):
+ # Open the image file
+    original_image = Image.open(image_path)
+
+    # Rotate the image 90 degrees
+    rotated_image_90 = original_image.rotate(90)
+    rotated_image_90.save(f'{save_path}/rotated_image_90_{image_path.split("/")[-1]}')
+
+    # Rotate the image 180 degrees
+    rotated_image_180 = original_image.rotate(180)
+    rotated_image_180.save(f'{save_path}/rotated_image_180_{image_path.split("/")[-1]}')
+
+    # Horizontal flip
+    flipped_horizontal_image = original_image.transpose(Image.FLIP_LEFT_RIGHT)
+    flipped_horizontal_image.save(f'{save_path}/flipped_horizontal_{image_path.split("/")[-1]}')
+
+    # Vertical flip
+    flipped_vertical_image = original_image.transpose(Image.FLIP_TOP_BOTTOM)
+    flipped_vertical_image.save(f'{save_path}/flipped_vertical_{image_path.split("/")[-1]}')
+
+
+rotate_and_flip_image("public/tiger.jpg", "public")
+
+
+def apply_filters(image_path, save_path):
+    original_image = Image.open(image_path)
+    blurred_image = original_image.filter(ImageFilter.BLUR)
+    sharpened_image = original_image.filter(ImageFilter.SHARPEN)
+    blurred_image.save(f'{save_path}/blurred_image.jpg')
+    sharpened_image.save(f'{save_path}/sharpened_image.jpg')
+
+apply_filters("public/tiger.jpg", "public")
+
